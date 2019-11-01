169aea6985c8519fe83cb6c0bdae187f89a605ce
ernest <echakhoyan@bluip.com>
2019-11-02 03:19:01 +0400

    Added tests for increment decremnt buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component{
-  render() {
-    return (
-        <div data-test="component-app">
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
 
-        </div>
-    );
-  }
+    incrementCounter = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+
+    decrementCounter = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    };
+
+    render() {
+        return (
+            <div data-test="component-app">
+                <h2 data-test="app-counter">The counter is currently {this.state.counter}</h2>
+                {this.state.counter < 0 && <h3 style={{color: "red"}}>Counter can not be below 0</h3>}
+                <button data-test="app-button" onClick={this.incrementCounter}>Increment counter</button>
+                <button data-test="decrement-button" onClick={this.decrementCounter}>Decrement counter</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,83 @@
-import React from 'react';
-import Enzyme, {shallow} from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import App from './App';
+import App from "./App";
 
 Enzyme.configure({
     adapter: new EnzymeAdapter()
 });
 
-it('> should renders without crashing', () => {
-    const wrapper = shallow(<App/>);
-    const appComponent = wrapper.find("[data-test='component-app']");
+const setup = (props = {}, state = null) => {
+    const wrapper = shallow(<App />);
+    if (state) {
+        wrapper.setState(state);
+    }
+    return wrapper;
+};
+
+const findBtTestAttr = (component, attr) => {
+    return component.find(`[data-test="${attr}"]`);
+};
+
+it("> should renders without crashing", () => {
+    const wrapper = setup();
+    const appComponent = findBtTestAttr(wrapper, "component-app");
     expect(appComponent.length).toBe(1);
 });
 
-it('> should render increment button', () => {
-
+it("> should render increment button", () => {
+    const wrapper = setup();
+    const button = findBtTestAttr(wrapper, "app-button");
+    expect(button.length).toBe(1);
 });
 
-it('> should render counter display', () => {
-
+it("> should render counter display", () => {
+    const wrapper = setup();
+    const counter = findBtTestAttr(wrapper, "app-counter");
+    expect(counter.length).toBe(1);
 });
 
-it('> counter starts at 0', () => {
-
+it("> counter starts at 0", () => {
+    const wrapper = setup();
+    const initialCounterState = wrapper.state("counter");
+    expect(initialCounterState).toBe(0);
 });
 
-it('> clicking button increments counter display', () => {
+it("> clicking button increments counter display", () => {
+    const counter = 7;
+    const wrapper = setup(null, { counter });
 
+    const button = findBtTestAttr(wrapper, "app-button");
+    button.simulate("click");
+
+    const displayCounter = findBtTestAttr(wrapper, "app-counter");
+    expect(displayCounter.text()).toContain(counter + 1);
 });
 
+it("> should render decrement button", () => {
+    const wrapper = setup();
+    const decrementButton = findBtTestAttr(wrapper, "decrement-button");
+
+    expect(decrementButton.length).toBe(1);
+});
+
+it("> clicking button decrements counter display", () => {
+    const counter = 7;
+    const wrapper = setup(null, { counter });
+
+    const decrementButton = findBtTestAttr(wrapper, "decrement-button");
+    decrementButton.simulate("click");
+
+    const displayCounter = findBtTestAttr(wrapper, "app-counter");
+    expect(displayCounter.text()).toContain(counter - 1);
+});
+
+it("> should not be negative counter", () => {
+    const counter = 1;
+    const wrapper = setup(null, { counter });
+
+    const decrementButton = findBtTestAttr(wrapper, "decrement-button");
+    decrementButton.simulate("click");
+
+    expect(wrapper.state().counter).toBeGreaterThanOrEqual(0);
+});
